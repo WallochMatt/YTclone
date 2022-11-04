437a39b5dafa811647d41b6155a646080b726d8b
unknown <arturodiazmay@gmail.com>
2022-11-04 11:38:58 -0500

    Added Video Description and Title to VideoPage

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,6 +4,7 @@ const VideoPlayer = (props) => {
     console.log('videoPlayer video_id', props.video_id)
     let test= `https://www.youtube.com/embed/${props.video_id}?autoplay=1`
 
+    
 
     return(
         <iframe id="ytplayer" type="text/html" width="640" height="360"
--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -3,8 +3,19 @@ import { useParams } from "react-router-dom";
 
 const VideoPage = (props) => {
     const {videoId} = useParams()
+    console.log("before filter videos", props.videos)
+    let selectedVideo = props.videos.filter(video => video.id.videoId === videoId)
+    console.log("after filter video", selectedVideo)
+
     return ( 
-        <VideoPlayer video_id={videoId}/>
+        <div>
+            <div><VideoPlayer video_id={videoId}/></div>
+            <div>
+                <p>{selectedVideo[0].snippet.title}</p>
+                <p>{selectedVideo[0].snippet.description}</p>
+            </div>
+        </div>
+
     );
 }
 
