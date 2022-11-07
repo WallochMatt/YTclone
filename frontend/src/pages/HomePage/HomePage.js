b662da4dd8545c666eedead9683dbeda1f8fbdd4
unknown <arturodiazmay@gmail.com>
2022-11-07 17:03:59 -0600

    feat: Added Videos To Home Page To Show Up on Render

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,40 +1,49 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
-
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import keys from "../../API_Keys.json"
+import "./HomePage.css"
 
-const HomePage = () => {
+const HomePage = (props) => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  let navigate = useNavigate();
+
+  
+  function handleClick(event, video){
+    event.preventDefault();
+    props.setSelectedVideo(video);
+    navigate(`/watch/${video.id.videoId}`);
+  }
+    
+    async function getHomeVids(){
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=trending&key=${keys.googleAPIKey}&part=snippet&maxResults=12`)
+      props.setVideos(response.data.items)
+    }
   
   useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
-  }, [token]);
+    getHomeVids()
+
+    
+  }, []);
+
   return (
-    <div className="container">
-      <h1>Welcome back, {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
+    <div>
+      <h1 className="home-text">Welcome Back {user.username}! Check Out What's Trending!</h1>
+      <ul className="wrapper"> 
+                {props.videos.map((video, index) => {
+                    return(                    
+                            <li key={index} >
+                                <button className="vid-space" onClick={(e) => handleClick(e, video)}><p className="order">{video.snippet.title}</p>
+                                <img src={video.snippet.thumbnails.medium.url} /></button>
+                            </li>                    
+                    )
+                })}
+            </ul>
     </div>
   );
 };
