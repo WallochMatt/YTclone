912a37d316cd72a945ce6800d75f5205e4a37628
WallochMatt <matthewrwalloch@gmail.com>
2023-05-18 17:06:19 -0500

    feat: Made edits to dockerfile and compose to utilize nginx as the free server is limited

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE IF NOT EXISTS yt_db;
+-- CREATE DATABASE IF NOT EXISTS yt_db;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'yt_db')
+BEGIN
+    CREATE DATABASE database_name;
+END;
+GO
